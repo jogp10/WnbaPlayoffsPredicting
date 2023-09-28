01c477692eef38629d2be95990538a1080251d63
João Matos <jcarvalhaismatos@gmail.com>
2023-09-28 15:02:04 +0100

    convert to dataframes

--- db_utils.py
+++ db_utils.py
@@ -10,16 +10,16 @@ password = config["db_password"]
 database = config["db_database"]
 
 # SELECT
-SELECT = "SELECT * FROM " # + table_name
+SELECT = "SELECT * FROM "  # + table_name
 
 # INSERT
-INSERT = "INSERT INTO " # + table_name + " VALUES " + values
+INSERT = "INSERT INTO "  # + table_name + " VALUES " + values
 
 # UPDATE
-UPDATE = "UPDATE " # + table_name + " SET " + column_name + " = " + value
+UPDATE = "UPDATE "  # + table_name + " SET " + column_name + " = " + value
 
 # DELETE
-DELETE = "DELETE FROM " # + table_name + " WHERE " + column_name + " = " + value
+DELETE = "DELETE FROM "  # + table_name + " WHERE " + column_name + " = " + value
 
 
 # Functions so I can reutilize the code
@@ -33,35 +33,43 @@ def connect():
     cursor = connection.cursor()
     return connection, cursor
 
+
 def close(connection, cursor):
     cursor.close()
     connection.close()
+
 
 def execute(connection, cursor, query):
     cursor.execute(query)
     connection.commit()
     return cursor.fetchall()
 
+
 def fetch(connection, cursor, query):
     cursor.execute(query)
     return cursor.fetchall()
+
 
 def fetchone(connection, cursor, query):
     cursor.execute(query)
     return cursor.fetchone()
 
+
 def fetchmany(connection, cursor, query, size):
     cursor.execute(query)
     return cursor.fetchmany(size)
+
 
 def insert(connection, cursor, query, values):
     cursor.execute(query, values)
     connection.commit()
     return cursor.lastrowid
 
+
 def update(connection, cursor, query, values):
     cursor.execute(query, values)
     connection.commit()
+
 
 def delete(connection, cursor, query, values):
     cursor.execute(query, values)
